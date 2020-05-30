63f8ac481f09baa2beef8cb8cd430b926cdc7fc8
Wesley Jonathan Marcolino <wesleyjonatan@gmail.com>
2020-05-30 15:30:05 -0300

    Move jetpack views into compose-view module

---
+++ compose-view/src/main/java/br/com/wesjon/compose_view/ComposeApp.kt
@@ -0,0 +1,8 @@
+package br.com.wesjon.compose_view
+
+import androidx.compose.Composable
+
+@Composable
+fun ComposeApp() {
+    HomeScreen()
+}
---
+++ compose-view/src/main/java/br/com/wesjon/compose_view/HomeScreen.kt
@@ -0,0 +1,14 @@
+package br.com.wesjon.compose_view
+
+import androidx.compose.Composable
+import androidx.ui.foundation.Text
+import androidx.ui.material.MaterialTheme
+import androidx.ui.tooling.preview.Preview
+
+@Preview
+@Composable
+fun HomeScreen() {
+    MaterialTheme {
+        Text(text = "xpto")
+    }
+}
