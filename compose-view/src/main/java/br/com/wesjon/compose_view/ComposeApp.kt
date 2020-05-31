e2768d9d0ba2c38cbc52f5636b535e46921885b8
Wesley Jonathan Marcolino <wesleyjonatan@gmail.com>
2020-05-31 16:00:19 -0300

    Add example for linear layout like layouts

--- compose-view/src/main/java/br/com/wesjon/compose_view/ComposeApp.kt
+++ compose-view/src/main/java/br/com/wesjon/compose_view/ComposeApp.kt
@@ -2,8 +2,17 @@ package br.com.wesjon.compose_view
 
 import androidx.compose.Composable
 import br.com.wesjon.compose_view.screen.HomeScreen
+import br.com.wesjon.compose_view.screen.LinearLayoutScreen
+import com.github.zsoltk.compose.router.Router
 
 @Composable
-fun ComposeApp() {
-    HomeScreen()
+fun ComposeApp(defaultRouting: Routing) {
+    Router("homescreen", defaultRouting) { backStack ->
+        when (val routing = backStack.last()) {
+            is Routing.HomeScreen -> HomeScreen {
+                backStack.push(Routing.ExampleLinearLayoutLike)
+            }
+            is Routing.ExampleLinearLayoutLike -> LinearLayoutScreen()
+        }
+    }
 }
---
+++ compose-view/src/main/java/br/com/wesjon/compose_view/Routing.kt
@@ -0,0 +1,6 @@
+package br.com.wesjon.compose_view
+
+sealed class Routing {
+    object HomeScreen : Routing()
+    object ExampleLinearLayoutLike : Routing()
+}
