906ed374cf1cb213eb0deb3ea9e68b5cc317819a
Wesley Jonathan Marcolino <w.jonathan.marcolino@accenture.com>
2020-08-27 17:41:15 -0300

    Upgrade to alpha 1

--- compose-app/src/main/java/br/com/wesjon/compose_view/screen/ScrollableListScreen.kt
+++ compose-app/src/main/java/br/com/wesjon/compose_view/screen/ScrollableListScreen.kt
@@ -1,16 +1,17 @@
 package br.com.wesjon.compose_view.screen
 
-import androidx.compose.Composable
-import androidx.ui.core.Modifier
-import androidx.ui.foundation.Box
-import androidx.ui.foundation.lazy.LazyColumnItems
-import androidx.ui.graphics.Color
-import androidx.ui.layout.Spacer
-import androidx.ui.layout.fillMaxWidth
-import androidx.ui.layout.height
-import androidx.ui.layout.padding
-import androidx.ui.unit.dp
+import androidx.compose.foundation.Box
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumnFor
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import br.com.wesjon.compose_view.extension.isOdd
+
 
 private val boxSize = 60.dp
 private val coolColors = listOf(
@@ -19,25 +20,24 @@ private val coolColors = listOf(
 
 @Composable
 fun ScrollableListScreen() {
-    LazyColumnItems(
-        items = (0..20).toList(),
-        modifier = Modifier.padding(horizontal = 16.dp)
-    ) { index ->
-        // Here is the equivalent to "onCreateViewHolder", build your listItem here
+    LazyColumnFor(items = (0..20).toList(),
+        modifier = Modifier.padding(horizontal = 16.dp),
+        itemContent = { index ->
+            // Here is the equivalent to "onCreateViewHolder", build your listItem here
 
-        if (index == 0) {
+            if (index == 0) {
+                Spacer(modifier = Modifier.height(8.dp))
+            }
+
+            // The equivalent of multiple ViewTypes can be expressed as conditions here
+            if (index.isOdd()) {
+                ListItemTypeA()
+            } else {
+                ListItemTypeB()
+            }
+
             Spacer(modifier = Modifier.height(8.dp))
-        }
-
-        // The equivalent of multiple ViewTypes can be expressed as conditions here
-        if (index.isOdd()) {
-            ListItemTypeA()
-        } else {
-            ListItemTypeB()
-        }
-
-        Spacer(modifier = Modifier.height(8.dp))
-    }
+        })
 }
 
 @Composable
